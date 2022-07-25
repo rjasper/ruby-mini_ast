040bf0c364295c59ae49ee044d6c3d14dc295387
Rico Jasper <jasper.rico@gmail.com>
2022-07-25 23:44:51 +0200

    Correct Lint/AmbiguousBlockAssociation

--- test/test_mini_ast.rb
+++ test/test_mini_ast.rb
@@ -8,28 +8,49 @@ class TestMiniAst < Minitest::Test
   end
 
   def test_simple_literals
-    assert_equal literal(1), build { 1 }
-    assert_equal literal('test'), build { 'test' }
-    assert_equal literal(nil), build { nil }
+    assert_equal(
+      literal(1),
+      build { 1 },
+    )
+
+    assert_equal(
+      literal('test'),
+      build { 'test' },
+    )
+
+    assert_equal(
+      literal(nil),
+      build { nil },
+    )
   end
 
   def test_complex_literals
-    assert_equal literal([literal(1), literal(2)]),
-      build { [1, 2] }
+    assert_equal(
+      literal([literal(1), literal(2)]),
+      build { [1, 2] },
+    )
 
-    assert_equal literal(literal(:foo) => literal(:bar)),
-      build { { foo: :bar } }
+    assert_equal(
+      literal(literal(:foo) => literal(:bar)),
+      build { { foo: :bar } },
+    )
   end
 
   def test_calls
-    assert_equal call(nil, :foo),
-      build { foo }
+    assert_equal(
+      call(nil, :foo),
+      build { foo },
+    )
 
-    assert_equal call(nil, :foo, literal(42)),
-      build { foo(42) }
+    assert_equal(
+      call(nil, :foo, literal(42)),
+      build { foo(42) },
+    )
 
-    assert_equal call(call(nil, :foo), :bar),
-      build { foo.bar }
+    assert_equal(
+      call(call(nil, :foo), :bar),
+      build { foo.bar },
+    )
   end
 
   private
