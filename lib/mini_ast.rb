02b72845457e7c2090cb6aa8eee0f4e805cd105f
Rico Jasper <jasper.rico@gmail.com>
2022-07-25 21:27:27 +0200

    Fix file loading

--- lib/mini_ast.rb
+++ lib/mini_ast.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "mini_ast/version"
+require 'mini_ast/builder'
+require 'mini_ast/call'
+require 'mini_ast/literal'
+require 'mini_ast/version'
 
 module MiniAst
   def self.build(&block)
