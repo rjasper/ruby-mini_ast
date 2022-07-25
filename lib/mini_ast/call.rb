928fd95898c037ceadd75564082518e53633a8f1
Rico Jasper <jasper.rico@gmail.com>
2022-07-25 23:54:50 +0200

    Implement Call#ary

--- lib/mini_ast/call.rb
+++ lib/mini_ast/call.rb
@@ -11,6 +11,10 @@ module MiniAst
       @block = block
     end
 
+    def ary
+      args.size
+    end
+
     def ==(other)
       return true if equal?(other)
 
@@ -39,18 +43,18 @@ module MiniAst
       case method
       when :!, :~
         # !foo
-        return "#{method}#{receiver}" if args.size == 0
+        return "#{method}#{receiver}" if ary == 0
       when :+@
         # +foo
-        return "+#{receiver}" if args.size == 0
+        return "+#{receiver}" if ary == 0
       when :-@
         # -foo
-        return "-#{receiver}" if args.size == 0
+        return "-#{receiver}" if ary == 0
       when :+, :-, :*, :/, :%, :**, :<, :>, :<=, :>=, :<=>, :==, :===, :!=,
         :=~, :!~, :&, :|, :^, :<<, :>>
 
         # foo + bar
-        return "(#{receiver} #{method} #{args.first})" if args.size == 1
+        return "(#{receiver} #{method} #{args.first})" if ary == 1
       when :[]
         # foo[a, b, ...]
         return "#{receiver}#{args}"
@@ -61,14 +65,14 @@ module MiniAst
 
       receiver_part = receiver.nil? ? '' : "#{receiver}."
 
-      if method.end_with?('=') && args.size == 1
+      if method.end_with?('=') && ary == 1
         # foo.bar = 42
 
         "(#{receiver_part}#{method[0..-2]} = #{args.first})"
       else
         # foo.bar OR foo.bar(arg1, arg2, ...)
 
-        args_part = args.size == 0 ? '' : "(#{args.map(&:to_s).join(', ')})"
+        args_part = ary == 0 ? '' : "(#{args.map(&:to_s).join(', ')})"
         "#{receiver_part}#{method}#{args_part}"
       end
     end
